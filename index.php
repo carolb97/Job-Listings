5c3b14ea9a4fb0d46f8f23847013e64e887dda0c
carolb97 <49376624+carolb97@users.noreply.github.com>
2023-02-15 05:39:49 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
+##### MAIN FILE #####
+
+//Include the auxiliary files
 require('app/app.php');
 
+//Import data and convert to json string
 $json = get_data();
 
+//Decode the json string
 $terms = json_decode($json);
 
+//Call main program
 view('index', $terms);
 
 
